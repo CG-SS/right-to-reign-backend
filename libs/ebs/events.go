79918f60967d54d4d35a45740242013d3a1f49b1
CG-SS <criguiss@uol.com.br>
2022-08-15 21:15:36 -0300

    Add omitempty

--- libs/ebs/events.go
+++ libs/ebs/events.go
@@ -25,24 +25,24 @@ type CreateCity struct {
 
 type UpdateCity struct {
 	ID                 uuid.UUID `json:"id"`
-	Name               *string   `json:"name"`
-	TownCenterLevel    *uint8    `json:"town_center_level"`
-	HouseLevel         *uint8    `json:"house_level"`
-	MillLevel          *uint8    `json:"mill_level"`
-	FarmLevel          *uint8    `json:"farm_level"`
-	LumberCampLevel    *uint8    `json:"lumber_camp_level"`
-	MiningCampLevel    *uint8    `json:"mining_camp_level"`
-	DockLevel          *uint8    `json:"dock_level"`
-	BarracksLevel      *uint8    `json:"barracks_level"`
-	PalisadeWallLevel  *uint8    `json:"palisade_wall_level"`
-	StoneWallLevel     *uint8    `json:"stone_wall_level"`
-	OutpostLevel       *uint8    `json:"outpost_level"`
-	MarketLevel        *uint8    `json:"market_level"`
-	BlacksmithLevel    *uint8    `json:"blacksmith_level"`
-	ArcheryRangeLevel  *uint8    `json:"archery_range_level"`
-	StableLevel        *uint8    `json:"stable_level"`
-	WatchTowerLevel    *uint8    `json:"watch_tower_level"`
-	UniversityLevel    *uint8    `json:"university_level"`
-	SiegeWorkshopLevel *uint8    `json:"siege_workshop_level"`
-	CastleLevel        *uint8    `json:"castle_level"`
+	Name               *string   `json:"name,omitempty"`
+	TownCenterLevel    *uint8    `json:"town_center_level,omitempty"`
+	HouseLevel         *uint8    `json:"house_level,omitempty"`
+	MillLevel          *uint8    `json:"mill_level,omitempty"`
+	FarmLevel          *uint8    `json:"farm_level,omitempty"`
+	LumberCampLevel    *uint8    `json:"lumber_camp_level,omitempty"`
+	MiningCampLevel    *uint8    `json:"mining_camp_level,omitempty"`
+	DockLevel          *uint8    `json:"dock_level,omitempty"`
+	BarracksLevel      *uint8    `json:"barracks_level,omitempty"`
+	PalisadeWallLevel  *uint8    `json:"palisade_wall_level,omitempty"`
+	StoneWallLevel     *uint8    `json:"stone_wall_level,omitempty"`
+	OutpostLevel       *uint8    `json:"outpost_level,omitempty"`
+	MarketLevel        *uint8    `json:"market_level,omitempty"`
+	BlacksmithLevel    *uint8    `json:"blacksmith_level,omitempty"`
+	ArcheryRangeLevel  *uint8    `json:"archery_range_level,omitempty"`
+	StableLevel        *uint8    `json:"stable_level,omitempty"`
+	WatchTowerLevel    *uint8    `json:"watch_tower_level,omitempty"`
+	UniversityLevel    *uint8    `json:"university_level,omitempty"`
+	SiegeWorkshopLevel *uint8    `json:"siege_workshop_level,omitempty"`
+	CastleLevel        *uint8    `json:"castle_level,omitempty"`
 }
